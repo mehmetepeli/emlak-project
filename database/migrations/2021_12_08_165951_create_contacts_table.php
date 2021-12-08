eb34780b5510e829ef4c387e2e618270ab08daa4
Mehmet Tepeli <mehmettepeli@gmail.com>
2021-12-08 20:33:02 +0300

    updated contacts migration

--- database/migrations/2021_12_08_165951_create_contacts_table.php
+++ database/migrations/2021_12_08_165951_create_contacts_table.php
@@ -15,6 +15,12 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('address');
+            $table->string('password');
             $table->timestamps();
         });
     }
