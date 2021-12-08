b405bc6fe802a1fb369d02e05f7c27a3fbeb365f
Mehmet Tepeli <mehmettepeli@gmail.com>
2021-12-08 20:20:57 +0300

    create user migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('agent');
+            $table->tinyInteger('client');
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
