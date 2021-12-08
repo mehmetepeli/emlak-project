bd2bd91144c2a9c68ffe65d51d142b2a39a787c2
Mehmet Tepeli <mehmettepeli@gmail.com>
2021-12-08 20:25:41 +0300

    updated appoinments migration

--- database/migrations/2021_12_08_165844_create_appoinments_table.php
+++ database/migrations/2021_12_08_165844_create_appoinments_table.php
@@ -15,6 +15,19 @@ class CreateAppoinmentsTable extends Migration
     {
         Schema::create('appoinments', function (Blueprint $table) {
             $table->id();
+            $table->integer('agent_id');
+            $table->integer('client_id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('from_postcode');
+            $table->string('to_postcode');
+            $table->string('distance');
+            $table->string('meet_date');
+            $table->string('meet_time');
+            $table->string('exit_time');
+            $table->string('return_time');
             $table->timestamps();
         });
     }
