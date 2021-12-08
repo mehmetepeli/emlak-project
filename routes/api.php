b854409c459f54faf28c028d9abb98586a6af9e9
Mehmet Tepeli <mehmettepeli@gmail.com>
2021-12-08 20:38:02 +0300

    Added Appoinments route in api router

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Appoinments;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//Public routes
+Route::get('/appoinments', [Appoinments::class, 'index']);
+
+//Private routes
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
